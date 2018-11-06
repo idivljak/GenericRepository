8e32f8cf1652c253e7c59829a717935f865fd6ec
Ivan Divljak <ivan93.ns@hotmail.com>
2018-11-06 14:23:46 +0100

    Initial commit

---
+++ GenericRepository/GenericRepository/GenericRepository.h
@@ -0,0 +1,19 @@
+//
+//  GenericRepository.h
+//  GenericRepository
+//
+//  Created by Ivan Divljak on 11/6/18.
+//  Copyright © 2018 Ivan Divljak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GenericRepository.
+FOUNDATION_EXPORT double GenericRepositoryVersionNumber;
+
+//! Project version string for GenericRepository.
+FOUNDATION_EXPORT const unsigned char GenericRepositoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GenericRepository/PublicHeader.h>
+
+
---
+++ GenericRepository/GenericRepository/GenericRepositoryProtocol.swift
@@ -0,0 +1,154 @@
+//
+//  GenericRepositoryProtocol.swift
+//  GenericRepository
+//
+//  Created by Ivan Divljak on 11/6/18.
+//  Copyright © 2018 Ivan Divljak. All rights reserved.
+//
+
+import Foundation
+
+struct GenericResponse<T> {
+    let success: Bool?
+    let item: T?
+    let statusCode: Int?
+    let error: Error?
+    let message: String?
+}
+
+extension GenericResponse {
+    init(genericResponse: GenericResponse? = nil, item: T? = nil) {
+        self.success = genericResponse?.success
+        self.item = item ?? genericResponse?.item
+        self.statusCode = genericResponse?.statusCode
+        self.error = genericResponse?.error
+        self.message = genericResponse?.message
+    }
+}
+
+//protocol GenericRepositoryProtocol {
+//    
+//    func genericCodableRequest<T: Codable >(value: T.Type, _ url: URL, method: HTTPMethod,
+//                                            parameters: Parameters?, encoding: ParameterEncoding,
+//                                            headers: HTTPHeaders?, codableDecoder: JSONDecoder?,
+//                                            completion: @escaping (GenericResponse<T>) -> Void)
+//    
+//    func genericJSONRequest(_ url: URL, method: HTTPMethod,
+//                            parameters: Parameters?, encoding: ParameterEncoding,
+//                            headers: HTTPHeaders?,
+//                            completion: @escaping (GenericResponse<Any>) -> Void)
+//    
+//    func genericStringRequest(_ url: URL, method: HTTPMethod,
+//                              parameters: Parameters?, encoding: ParameterEncoding,
+//                              headers: HTTPHeaders?,
+//                              completion: @escaping (GenericResponse<String>) -> Void)
+//}
+//
+//extension GenericRepositoryProtocol {
+//    
+//    func genericCodableRequest<T: Codable >(value: T.Type, _ url: URL, method: HTTPMethod = .get,
+//                                            parameters: Parameters? = nil, encoding: ParameterEncoding = URLEncoding.default,
+//                                            headers: HTTPHeaders? = nil, codableDecoder: JSONDecoder? = nil,
+//                                            completion: @escaping (GenericResponse<T>) -> Void) {
+//        let completionHandler: (_ response: DataResponse<T>) -> Void = { (response: DataResponse<T>) in
+//            switch response.result {
+//            case .success:
+//                completion(GenericResponse<T> (success: true, item: response.result.value,
+//                                               statusCode: response.response?.statusCode, error: nil, message: nil))
+//            case .failure(let error):
+//                print(error)
+//                completion(GenericResponse<T> (success: false, item: nil, statusCode: response.response?.statusCode,
+//                                               error: error,
+//                                               message: response.error?.localizedDescription))
+//            }
+//        }
+//        
+//        print("""
+//            *******************************
+//            REQUEST
+//            URL: \(url)
+//            METHOD: \(method.rawValue)
+//            HEADERS: \(headers ?? [:])
+//            BODY: \(parameters ?? [:])
+//            *******************************
+//            """
+//        )
+//        Alamofire.request(url, method: method, parameters: parameters, encoding: encoding,
+//                          headers: headers).validate().responseDecodableObject(keyPath: nil,
+//                                                                               decoder: codableDecoder ?? getDecoder(),
+//                                                                               completionHandler: completionHandler)
+//    }
+//    
+//    func genericStringRequest(_ url: URL, method: HTTPMethod,
+//                              parameters: Parameters?, encoding: ParameterEncoding = URLEncoding.default,
+//                              headers: HTTPHeaders? = nil,
+//                              completion: @escaping (GenericResponse<String>) -> Void) {
+//        let completionHandler: (_ response: DataResponse<String>) -> Void = { (response: DataResponse<String>) in
+//            switch response.result {
+//            case .success:
+//                completion(GenericResponse<String> (success: true, item: response.result.value,
+//                                                    statusCode: response.response?.statusCode, error: nil, message: nil))
+//            case .failure(let error):
+//                print(error)
+//                
+//                completion(GenericResponse<String> (success: false, item: nil, statusCode: response.response?.statusCode,
+//                                                    error: response.error,
+//                                                    message: response.error?.localizedDescription))
+//            }
+//        }
+//        print("""
+//            *******************************
+//            REQUEST
+//            URL: \(url)
+//            METHOD: \(method.rawValue)
+//            HEADERS: \(headers ?? [:])
+//            BODY: \(parameters ?? [:])
+//            *******************************
+//            """
+//        )
+//        Alamofire.request(url, method: method, parameters: parameters, encoding: encoding,
+//                          headers: headers).validate().responseString(completionHandler: completionHandler)
+//        
+//    }
+//    
+//    func genericJSONRequest(_ url: URL, method: HTTPMethod,
+//                            parameters: Parameters?, encoding: ParameterEncoding = URLEncoding.default,
+//                            headers: HTTPHeaders? = nil,
+//                            completion: @escaping (GenericResponse<Any>) -> Void) {
+//        let completionHandler: (_ response: DataResponse<Any>) -> Void = { (response: DataResponse<Any>) in
+//            switch response.result {
+//            case .success:
+//                completion(GenericResponse<Any> (success: true, item: response.result.value,
+//                                                 statusCode: response.response?.statusCode, error: nil, message: nil))
+//            case .failure(let error):
+//                print(error)
+//                
+//                completion(GenericResponse<Any> (success: false, item: nil, statusCode: response.response?.statusCode,
+//                                                 error: response.error,
+//                                                 message: response.error?.localizedDescription))
+//            }
+//        }
+//        print("""
+//            *******************************
+//            REQUEST
+//            URL: \(url)
+//            METHOD: \(method.rawValue)
+//            HEADERS: \(headers ?? [:])
+//            BODY: \(parameters ?? [:])
+//            *******************************
+//            """
+//        )
+//        Alamofire.request(url, method: method, parameters: parameters, encoding: encoding,
+//                          headers: headers).validate().responseJSON(completionHandler: completionHandler)
+//    }
+//    
+//    func getDecoder() -> JSONDecoder {
+//        let decoder = JSONDecoder()
+//        decoder.dateDecodingStrategy = .formatted(getDateFormatter())
+//        return decoder
+//    }
+//    
+//    func getDateFormatter() -> DateFormatter {
+//        return DateFormatter()
+//    }
+//}
